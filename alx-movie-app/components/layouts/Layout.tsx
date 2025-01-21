b9da772bf1fda9d570d1c922881b4674fe8b782f
Don Bosco <don965877@gmail.com>
2025-01-21 05:45:11 +0300

    Set up Header, Footer, Layout and landing page

---
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -0,0 +1,15 @@
+import { componentProps} from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export const Layout: React. FC<ComponentProps> = ({children}) => {
+    return(
+        <>
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
